d9e2932d48af8482e257e4f450c045de2646258c
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-23 09:33:17 -0800

    components added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shadin's Portfolio
         </a>
       </header>
     </div>
---
+++ src/components/AboutMe.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//CREATE FUNCTION
+
+//EXPORT FUNCTION
+
