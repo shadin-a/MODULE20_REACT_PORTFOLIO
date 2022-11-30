de62a9cdaf9c71a432a056318b0b91f3f38e92f1
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-29 16:56:03 -0800

    added image and desc for 1 proj

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,20 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {Card} from 'react-bootstrap';
+import WLMDO from './WLMDO.png';
 
 function Projects(){
     return(
         <Container>
         <Row>
-          <Col> <Card className="bg-dark text-white">
-      <Card.Img src="public/assets/wholetdawgout.png" alt="Card image" />
+          <Col> <Card className="bg-dark text-black">
+      <Card.Img src={WLMDO} alt="Card image" />
       <Card.ImgOverlay>
         <Card.Title>Nerd Knowledge Quiz</Card.Title>
         <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
+        Fullstack web-based quiz game. Built using Bootstrap, Node.js, and MySQL leveraging MVC architecture.
         </Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
+        <Card.Text>Link</Card.Text>
       </Card.ImgOverlay>
     </Card></Col>
           <Col> <Card className="bg-dark text-white">
@@ -31,8 +31,28 @@ function Projects(){
     </Card></Col>
         </Row>
         <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
+          <Col><Card className="bg-dark text-white">
+      <Card.Img src="holder.js/100px270" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+    </Card></Col>
+          <Col><Card className="bg-dark text-white">
+      <Card.Img src="holder.js/100px270" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+    </Card></Col>
         </Row>
       </Container>
     );
